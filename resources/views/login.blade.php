3bfa35b722da00a033fd332a72ccbf9e43a99ee6
RayvanoPutra <rayvano159@gmail.com>
2024-04-20 13:16:00 +0700

    Commit Pertama laravel

---
+++ resources/views/login.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/kelompok-3.css">
+
+    {{-- google fonts start--}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    {{-- google fonts end --}}
+    <title>Hadir</title>
+  </head>
+  <body>
+
+{{-- login page start --}}
+    <section class="login d-flex">
+      <div class="login-left w-100  d-flex justify-content-center align-items-center">
+          <div>
+              <div class="header">
+                  <h1>SELAMAT DATANG</h1>
+                  <p>Hari ini adalah kesempatan untuk membuat perbedaan dari hari kemarin.</p>
+              </div>
+              <div class="login-form">
+                  <label for="username" class="form-label">Username</label>
+                  <input type="email" class="form-control" id="username" placeholder="Masukkan username">
+  
+                  <label for="Password" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="Password" placeholder="Masukkan password">
+                  <button class="Masuk">Masuk</button>
+              </div>
+          </div>
+      </div>
+      <div class="login-right w-50 h-10">
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="1500" "">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="img/candi-1.jpg" class="d-block " alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="img/candi-2.jpg" class="d-block " alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="img/candi-3.jpg" class="d-block " alt="..." >
+              </div>
+            </div>
+          
+          </div>
+  </section>
+{{-- login page end --}}
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
